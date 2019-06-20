3f1c16480d1a8598c3d1becc4e49a9a6bb24ed48
joshuaholloway <jhollow6@asu.edu>
2019-06-20 13:48:47 -0500

    added database

--- main/apps/AmazonApp/views.py
+++ main/apps/AmazonApp/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render, redirect
-
 from django.shortcuts import render, HttpResponse
+from .models import Product
+
 def index(request):
-    return render(request, "AmazonApp/index.html")
-    # return HttpResponse("hello")
+
+  # TODO: Remove this initialization of the database
+
+  # TODO: Initialize database upon first use
+  # Product.objects.create(item="Shirt", price=19.99)
+  # Product.objects.create(item="Shoes", price=24.99)
+  # Product.objects.create(item="Cup",   price=4.99)
+  # Product.objects.create(item="Book",  price=49.99)
+
+
+
+  return render(request, "AmazonApp/index.html", {'products': Product.objects.all()})
+  # return HttpResponse("hello")
