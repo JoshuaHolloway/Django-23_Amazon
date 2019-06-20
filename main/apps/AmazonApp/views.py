6dea216980a7027fd3210aa6ac9b382bbc36ac1b
joshuaholloway <jhollow6@asu.edu>
2019-06-20 14:13:48 -0500

    added drop-down and button for quantity in HTML. Have not wired up yet.

--- main/apps/AmazonApp/views.py
+++ main/apps/AmazonApp/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product
-
+# ======================================================================================================================
 def index(request):
-
-  # TODO: Remove this initialization of the database
-
   # TODO: Initialize database upon first use
   # Product.objects.create(item="Shirt", price=19.99)
   # Product.objects.create(item="Shoes", price=24.99)
   # Product.objects.create(item="Cup",   price=4.99)
   # Product.objects.create(item="Book",  price=49.99)
 
-
-
   return render(request, "AmazonApp/index.html", {'products': Product.objects.all()})
-  # return HttpResponse("hello")
+# ======================================================================================================================
+
+# ======================================================================================================================
+
+# ======================================================================================================================
